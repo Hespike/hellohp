1ba8d15cfcee8dbe3223b859dedd6eb6e52dc238
Sárdi András <sardi.andras@gmail.com>
2021-04-17 15:57:45 +0200

    Create hello.php

---
+++ hello.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="hu">
+  <head>
+    <title>Az első PHP programom</title>
+    <meta charset="UTF-8"/>
+  </head>
+  <body>
+    <?php 
+      echo "Hello World!"; 
+    ?>
+  </body>
+</html>
